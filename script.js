9d296886f440e17072417a5fd871626658d905ff
H.FoxDev <intira.vinit@gmail.com>
2023-02-08 13:30:45 +0700

    Array Filter

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-// Array Map
+// Array Filter
 
-const numbers = [10, 20, 30, 40]
-console.log(`numbers : ${numbers}`)
+const data = [10, 20, 30, 40]
 
-const new_numbers = numbers.map(e => e * 2)
-console.log(`new_numbers : ${new_numbers}`)
+const result = data.filter(e => e > 25)
+console.log(result)
 
-const data = ["Rainy", "Sunny", "Windy", "Sunny", "Windy", "Rainy"]
-const result = data.map((e, i) => {
-    return `Day ${i+1} is ${e}`
-})
-
-result.forEach(e => console.log(e))
-
-const obj = [
-    {day: "01/06/2023", weather: "Sunny", temp: 32},
-    {day: "02/06/2023", weather: "Rainy", temp: 28},
-    {day: "03/06/2023", weather: "Cold", temp: 24}
+const employees = [
+    {name: "Kong", salary: 25000, department: "Programmer"},
+    {name: "Jojo", salary: 30000, department: "Marketing"},
+    {name: "Nut", salary: 12000, department: "Apprentice"},
+    {name: "Bel", salary: 50000, department: "Marketing"},
+    {name: "Joy", salary: 12000, department: "Apprentice"},
 ]
 
-const weather = obj.map(e => e.weather)
-console.log(weather)
+const result_emp = employees.filter(e => e.department === "Apprentice")
+console.log(result_emp)
