e38fd2b7af46ac54742f84222ef9f61636bc8a70
H.FoxDev <intira.vinit@gmail.com>
2023-02-08 13:16:58 +0700

    Array Map

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-// indexOf, find, findIndex
+// Array Map
 
-const colors = ["Red", "Green", "Blue", "Purple", "White"]
+const numbers = [10, 20, 30, 40]
+console.log(`numbers : ${numbers}`)
 
-console.log(`Red is index ${colors.indexOf("Red")}`)
-console.log(`Blue is index ${colors.indexOf("Blue")}`)
+const new_numbers = numbers.map(e => e * 2)
+console.log(`new_numbers : ${new_numbers}`)
 
-console.log(`findIndex Green in colors : ${colors.findIndex(e => e==="Green")}`)
+const data = ["Rainy", "Sunny", "Windy", "Sunny", "Windy", "Rainy"]
+const result = data.map((e, i) => {
+    return `Day ${i+1} is ${e}`
+})
 
-console.log(colors.find(e => e==="Green"))
-console.log(colors.find(e => e==="Black"))
+result.forEach(e => console.log(e))
 
+const obj = [
+    {day: "01/06/2023", weather: "Sunny", temp: 32},
+    {day: "02/06/2023", weather: "Rainy", temp: 28},
+    {day: "03/06/2023", weather: "Cold", temp: 24}
+]
+
+const weather = obj.map(e => e.weather)
+console.log(weather)
